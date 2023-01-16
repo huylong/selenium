a4a192319f17fe1fbcdad1f8382e554273e7f439
Huy Pham <huy.pham4@ntq-solution.com.vn>
2023-01-16 16:24:51 +0700

    first commit

---
+++ install/init.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Install Selenium + Chrome + Java Centos
+
+#================================================
+# Variable
+#================================================
+set -e
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#================================================
+# Check sudo user
+#================================================
+
+if [[ "$EUID" -ne 0 ]]; then
+    printf "${GREEN}Please run as root or sudo.\n${NC}"
+    exit 1;
+fi
+
+#============================================
+# UnInstall Google Chrome
+#============================================
+ls -a | grep "google-chrome-" | grep -v grep | awk '{print $1}' | xargs -r rm
+yum list installed | grep "google-chrome" | grep -v grep | awk '{print $1}' | xargs -r yum -y remove
+
+#============================================
+# Install Google Chrome
+#============================================
+wget --no-check-certificate --no-verbose https://git.ntq.solutions/huy.pham4/selenium/-/raw/master/install/google-chrome-stable-108.0.5359.124-1.x86_64.rpm
+yum -y install google-chrome-stable-108.0.5359.124-1.x86_64.rpm
+#============================================
+# Install Google Chromedriver
+#============================================
+ls -a | grep "chromedriver_linux64" | grep -v grep | awk '{print $1}' | xargs -r rm
+wget https://git.ntq.solutions/huy.pham4/selenium/-/raw/master/install/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv -f chromedriver /usr/local/bin
+perl -pi -e 's/cdc_/dog_/g' /usr/local/bin/chromedriver
+#============================================
+# Install Selenium
+#============================================
+rm -rf selenium-server-standalone-3.6.0.jar
+wget https://git.ntq.solutions/huy.pham4/selenium/-/raw/master/install/selenium-server-standalone-3.9.1.jar
+mv selenium-server-standalone-3.9.1.jar /usr/local/bin
+#============================================
+
+systemctl enable httpd.service
+
+#============================================
+# Install Supervisor
+#============================================
+perl -pi -e 's/selenium-server-standalone-3.6.0.jar -enablePassThrough false/selenium-server-standalone-3.9.1.jar/g' /etc/supervisord.conf
+supervisorctl reload
+rm -f google-chrome-stable-108.0.5359.124-1.x86_64.rpm
+service firewalld stop
+#============================================
+# Install Supervisor
+#============================================
+systemctl restart supervisord
+#============================================
+# Echo
+#============================================
+printf "${GREEN}java -jar /usr/local/bin/selenium-server-standalone-3.9.1.jar -timeout 60 & \n${NC}"
+printf "${GREEN}vncserver \n${NC}"
